8c522c129a3b8053186cfca7fdd30d4a0269917e
brad <brad@brad.tw>
2017-01-22 09:47:20 +0800

    v3

--- app/src/main/java/tw/org/iii/brad10/MainActivity.java
+++ app/src/main/java/tw/org/iii/brad10/MainActivity.java
@@ -1,16 +1,24 @@
 package tw.org.iii.brad10;
 
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView tv;
+    private UIHnadler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        handler = new UIHnadler();
+        tv = (TextView)findViewById(R.id.tv);
     }
 
     public void test1(View v){
@@ -22,11 +30,20 @@ public class MainActivity extends AppCompatActivity {
         public void run() {
             for (int i=0; i<10; i++){
                 Log.v("brad", "i = " + i);
+                handler.sendEmptyMessage(i);
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
                 }
             }
+        }
+    }
+
+    private class UIHnadler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            tv.setText("i = " + msg.what);
         }
     }
 
