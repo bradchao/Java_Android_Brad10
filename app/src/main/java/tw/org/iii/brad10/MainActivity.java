3bdc66e302b9fb8a1708e5f8ce663161f3144d72
brad <brad@brad.tw>
2017-01-22 09:24:37 +0800

    v2

--- app/src/main/java/tw/org/iii/brad10/MainActivity.java
+++ app/src/main/java/tw/org/iii/brad10/MainActivity.java
@@ -2,6 +2,8 @@ package tw.org.iii.brad10;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void test1(View v){
+        new MyThread().start();
+    }
+
+    private class MyThread extends Thread {
+        @Override
+        public void run() {
+            for (int i=0; i<10; i++){
+                Log.v("brad", "i = " + i);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                }
+            }
+        }
+    }
+
+
 }
