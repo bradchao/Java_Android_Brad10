7bd530748fd6b973c7a89368b87691261db5d331
brad <brad@brad.tw>
2017-01-22 09:53:29 +0800

    v4

--- app/src/main/java/tw/org/iii/brad10/MainActivity.java
+++ app/src/main/java/tw/org/iii/brad10/MainActivity.java
@@ -24,6 +24,21 @@ public class MainActivity extends AppCompatActivity {
     public void test1(View v){
         new MyThread().start();
     }
+    public void test2(View v){
+        new Thread(){
+            @Override
+            public void run() {
+                for (int i=0; i<10; i++){
+                    Log.v("brad", "i = " + i);
+                    handler.sendEmptyMessage(i);
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                    }
+                }
+            }
+        }.start();
+    }
 
     private class MyThread extends Thread {
         @Override
